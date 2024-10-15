e0616cab6533445da28d1424706a51c46848a188
unknown <panahpourmohamadmahdi62@gmail.com>
2024-10-15 19:12:05 +0300

    second commit

---
+++ lab.py
@@ -0,0 +1,17 @@
+import numpy
+
+class nn():
+    """
+    In NN class there are different ML models and functions available that helps you build and train your ML model.
+    """
+    def __init__(self):
+        layers = []
+        init_method = None
+
+
+    def add_vanila(number_of_neurons):
+        number_of_layers +=1
+        layers.append(number_of_neurons)
+
+    
+
