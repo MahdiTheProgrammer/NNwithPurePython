26bd7f521bb17c65628c150e2ae906ba8853ce28
unknown <panahpourmohamadmahdi62@gmail.com>
2024-10-16 13:17:05 +0300

    commit

--- lab.py
+++ lab.py
@@ -1,4 +1,5 @@
-import numpy
+import numpy as np
+import math
 
 class Vanila:
     """
@@ -20,12 +21,15 @@ class Vanila:
             raise ValueError("In order to add a layer you have to set the initializtion method")
 
         if self.layers:
+            previous_neurons = self.layers[-1]
             if self.init_method == "xavier":
-                previous_neurons = self.layers[-1]
-                # weights = np.random.uniform  Continue from here!
-            
+                weights = np.random.uniform(-(np.sqrt(6/(previous_neurons+number_of_neurons))), np.sqrt(6/(previous_neurons+number_of_neurons))), (previous_neurons, number_of_neurons)
+            else:
+                weights = np.random.randn(previous_neurons, number_of_neurons) * np.sqrt(2 / previous_neurons) #IDK why
 
-
+            self.weights.append(weights)
+        
+        self.biases.append(np.zeros(number_of_neurons,1))
         self.layers.append(number_of_neurons)
 
     def properties(self):
@@ -34,7 +38,22 @@ class Vanila:
             print("The initializtion method is: "+ str(self.init_method))
 
     def set_init_method(self, init_method=""):
+        """
+        Choose between 'xavier' or 'he' initialization method.
+        """
         if init_method == "xavier" or init_method == "he":
             self.init_method = init_method
         else:
             raise ValueError("For vanila model only xavier and he is available at this moment")
+
+class Activations:
+    def sigmoid(self, x):
+        y = 1 / (1 + np.exp(-x))  
+        return y
+
+    def tanh(self, x):
+        y = (np.exp(x) - np.exp(-x)) / (np.exp(x) + np.exp(-x))  
+        return y
+
+    def ReLU(self, x):
+        return np.maximum(0, x)  
